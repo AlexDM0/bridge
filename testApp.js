5f1f5d18dc221bd3bd0cdf36bd7ebad1159dd319
Alex de Mulder <alexdemulder@gmail.com>
2015-03-03 19:40:30 +0100

    changed port

--- testApp.js
+++ testApp.js
@@ -5,12 +5,16 @@
 var eve = require('evejs');
 var RPCAgent = require('./agents/RPCAgent');
 
+var port = process.env.PORT;
+console.log(port);
+
 eve.system.init({
   transports: [
     {
       type: 'http',
-      url: 'https://mighty-waters-2966.herokuapp.com:3000/agents/:id',
+      url: 'https://mighty-waters-2966.herokuapp.com:' + port + '/agents/:id',
       localShortcut: false,
+      port:port,
       default: true
     }
   ]
@@ -21,7 +25,7 @@ var agent1 = new RPCAgent('agent1');
 //var agent2 = new RPCAgent('agent2');
 
 // send a message to agent1
-//agent2.askToAdd('https://mighty-waters-2966.herokuapp.com:3000/agents/agent1', {a: 1, b: 2});
+//agent2.askToAdd('https://mighty-waters-2966.herokuapp.com:' + port + '/agents/agent1', {a: 1, b: 2});
 
 // catch error, send a message to a non-existing agent will fail
 //agent2.rpc.request('agent4', {method: 'add', params: {a: 1, b: 2}})
