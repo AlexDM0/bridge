3297040a57d49d193925c3a15e826cf8730ed4ab
Alex de Mulder <alexdemulder@gmail.com>
2015-03-03 19:25:35 +0100

    changed addresses

--- testApp.js
+++ testApp.js
@@ -9,8 +9,7 @@ eve.system.init({
   transports: [
     {
       type: 'http',
-      url: 'http://127.0.0.1:3000/agents/:id',
-      remoteUrl: 'http://127.0.0.1:3000/agents/:id',
+      url: 'https://mighty-waters-2966.herokuapp.com:3000/agents/:id',
       localShortcut: false,
       default: true
     }
@@ -22,7 +21,7 @@ var agent1 = new RPCAgent('agent1');
 var agent2 = new RPCAgent('agent2');
 
 // send a message to agent1
-agent2.askToAdd('http://127.0.0.1:3000/agents/agent1', {a: 1, b: 2});
+agent2.askToAdd('https://mighty-waters-2966.herokuapp.com:3000/agents/agent1', {a: 1, b: 2});
 
 // catch error, send a message to a non-existing agent will fail
 agent2.rpc.request('agent4', {method: 'add', params: {a: 1, b: 2}})
